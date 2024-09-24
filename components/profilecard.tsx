87ff5817310682fa606a1cb5778e9f7c60eb0cca
AkimZmerli <akimdotzmerli@outlook.com>
2024-09-24 16:29:10 +0200

    boilderplate card

---
+++ components/profilecard.tsx
@@ -0,0 +1,7 @@
+export function ProfileCard() {
+  return (
+    <div className="w-full h-screen bg-slate-500">
+      <div className="flex justify-center align-middle border-4 rounded-lg"></div>
+    </div>
+  );
+}
