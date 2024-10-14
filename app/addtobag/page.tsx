ea294c1594225d6f0d9c7cf2409dbcc827a5e707
AkimZmerli <akimdotzmerli@outlook.com>
2024-10-14 14:45:09 +0200

    themes

--- app/addtobag/page.tsx
+++ app/addtobag/page.tsx
@@ -4,8 +4,15 @@ import { changeTheme } from "@/utils/helper";
 import { Switch } from "@/components/ui/switch";
 
 export default function addToBag() {
+  // default
+
   return (
     <div className="h-screen grid place-items-center bg-primary">
+      <Switch
+        onCheckedChange={(checked) =>
+          changeTheme(checked ? "theme1" : "theme2")
+        }
+      />
       <div>
         <button
           className="py-2 px-8 bg-buttons text-typography m-2"
